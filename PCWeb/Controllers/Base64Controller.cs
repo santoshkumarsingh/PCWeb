fb4cd4436949230c0f276c606f5945689523a176
santosh kumar singh <santosh.ksingh03@gmail.com>
2013-08-18 18:16:57 +0530

    google

---
+++ PCWeb/Controllers/Base64Controller.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System.Web.Http;
+
+namespace PCWeb.Controllers
+{
+    public class Base64Controller : ApiController
+    {
+        //
+        // GET: /Base64/
+        [HttpGet]
+        public string IsValid()
+        {
+            return "santosh";
+        }
+
+    }
+}
