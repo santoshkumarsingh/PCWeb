8326fe58d37d4eed982718ec14b5388d5a5cd74f
santosh kumar singh <santosh.ksingh03@gmail.com>
2013-08-30 19:02:14 +0530

    ceaser cipher

---
+++ PCWeb/Controllers/CryptoController.cs
@@ -0,0 +1,19 @@
+﻿using PCWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PCWeb.Controllers
+{
+    public class CryptoController : Controller
+    {
+        public ActionResult CeaserCipher()
+        {
+            InputViewModel viewModel = new InputViewModel();
+            return View(viewModel);
+        }
+
+    }
+}
--- PCWeb/Scripts/ProgrammersCalculator.js
+++ PCWeb/Scripts/ProgrammersCalculator.js
@@ -100,3 +100,39 @@ function TestIt(fm, operation) {
 
     return true;
 }
+
+
+function doCrypt(isDecrypt) {
+    var shiftText = document.getElementById("Output").value;
+    if (!/^-?\d+$/.test(shiftText)) {
+        alert("Shift is not an integer");
+        return;
+    }
+    var key = parseInt(shiftText, 10);
+    if (key < 0 || key >= 26) {
+        alert("Shift is out of range");
+        return;
+    }
+    if (isDecrypt)
+        key = (26 - key) % 26;
+    var textElem = document.getElementById("Input");
+    textElem.value = crypt(textElem.value, key);
+}
+
+
+/*
+ * Returns the result of having each letter of the given text shifted forward by the given key, with wraparound. Case is preserved, and non-letters are unchanged.
+ * Examples:
+ *   crypt("abz", 1) = "bca"
+ *   crypt("THe 123 !@#$", 13) = "GUr 123 !@#$"
+ */
+function crypt(input, key) {
+    var output = "";
+    for (var i = 0; i < input.length; i++) {
+        var c = input.charCodeAt(i);
+        if (c >= 65 && c <= 90) output += String.fromCharCode((c - 65 + key) % 26 + 65);  // Uppercase
+        else if (c >= 97 && c <= 122) output += String.fromCharCode((c - 97 + key) % 26 + 97);  // Lowercase
+        else output += input.charAt(i);  // Copy
+    }
+    return output;
+}
