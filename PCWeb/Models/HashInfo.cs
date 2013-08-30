aa9353d94009207019b91ae0f7a3c553102b9d62
santosh kumar singh <santosh.ksingh03@gmail.com>
2013-08-30 19:17:53 +0530

    Info css

--- PCWeb/Models/HashInfo.cs
+++ PCWeb/Models/HashInfo.cs
@@ -6,14 +6,19 @@ using System.Xml.Linq;
 
 namespace PCWeb.Models
 {
+
     public class HashInfo
     {
+        public static string InnerXml(XElement thiz)
+        {
+            return thiz.Nodes().Aggregate(string.Empty, (element, node) => element += node.ToString());
+        }
       static  string FILEPATH = HttpContext.Current.Server.MapPath(@"~/App_Data/HashingInfo.xml");
         public static string GetHashInfo(string type)
         {
             var xmlDoc = (from x in XDocument.Load(FILEPATH).Descendants("HashAlgo")
                          where x.Attribute("Type").Value == type
-                         select x.Element("Description").Value).FirstOrDefault();
+                          select InnerXml(x.Element("Description"))).FirstOrDefault();
             return xmlDoc;
                    
         }
